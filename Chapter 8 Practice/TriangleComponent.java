b46e5a7548c868a2921cb94efdb1f567e978fd19
dwlim12 <dwlim@NN123-HA-23.HS.sd203.org>
2016-02-16 10:30:20 -0600

    Almost Done

--- Chapter 8 Practice/TriangleComponent.java
+++ Chapter 8 Practice/TriangleComponent.java
@@ -6,19 +6,44 @@ import javax.swing.JComponent;
 public class TriangleComponent extends JComponent
 {
     private Ellipse2D dot;
+    private Line2D line1;
+    private Line2D line2;
+    private Line2D line3;
+    public int count;
     /**
      * Default constructor for objects of class DrawTriangle
      */
     public TriangleComponent()
     {
         dot = new Ellipse2D.Double();
+        line1 = new Line2D.Double();
+        line2 = new Line2D.Double();
+        line3 = new Line2D.Double();
+        count = 0;
     }
     
     public void paintComponent(Graphics g)
     {
         Graphics2D g2 = (Graphics2D) g;
-        g2.draw(dot);
-        g2.fill(dot);
+        if (this.count == 1)
+        {g2.draw(dot);
+         g2.fill(dot);}
+        if (this.count == 2)
+        {g2.draw(line1);}
+        if (this.count == 3)
+        {g2.draw(line2);
+         g2.draw(line3);}
+    }
+    
+    public void addPoint()
+    {
+        if (count < 3)
+        {count++;}
+        else
+        {
+            count = 0;
+            count ++;
+        }
     }
     
     public void drawDot(int x, int y)
@@ -29,6 +54,14 @@ public class TriangleComponent extends JComponent
     
     public void drawLine(int x, int y, int a, int b)
     {
-        
+        line1.setLine(x,y,a,b);
+        repaint();
+    }
+    
+    public void drawTriangle(int x, int y, int a, int b, int c, int d)
+    {
+        line2.setLine(x,y,c,d);
+        line3.setLine(a,b,c,d);
+        repaint();
     }
 }
--- Chapter 8 Practice/TriangleFrame.java
+++ Chapter 8 Practice/TriangleFrame.java
@@ -1,13 +1,14 @@
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
 import javax.swing.JFrame;
-import java.util.Arrays;
+import java.util.ArrayList;
 public class TriangleFrame extends JFrame
 {
     private static final int FRAME_WIDTH = 300;
     private static final int FRAME_HEIGHT = 400;
     private TriangleComponent scene;
-    private int[][] points;
+    private ArrayList<Integer> points;
+    private int count;
     
     class MousePressListener implements MouseListener
     {
@@ -15,7 +16,15 @@ public class TriangleFrame extends JFrame
         {
             int x = event.getX();
             int y = event.getY();
-            scene.drawDot(x,y);
+            points.add(x);
+            points.add(y);
+            scene.addPoint();
+            if (scene.count == 1)
+            {scene.drawDot(x,y);}
+            if (scene.count == 2)
+            {scene.drawLine(points.get(0),points.get(1),x,y);}
+            if (scene.count == 3)
+            {scene.drawTriangle(points.get(0),points.get(1),points.get(2),points.get(3),x,y);}
         }
         public void mouseReleased(MouseEvent event) {}
         public void mouseClicked(MouseEvent event) {}
@@ -25,7 +34,8 @@ public class TriangleFrame extends JFrame
     
     public TriangleFrame()
     {
-        points = new int[3][3];
+        points = new ArrayList<Integer>(6);
+        count = 0;
         
         scene = new TriangleComponent();
         add(scene);
